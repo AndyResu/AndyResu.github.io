4176f508df646892c701efc1039a6d21d93f7d37
real name genij <zgxzgx@gmail.com>
2023-03-25 12:01:47 -0500

    feat(wedding): header and footer abstraction

---
+++ footer.js
@@ -0,0 +1,18 @@
+let footer = `
+    <footer class="page-footer blue-grey darken-3 semi-transparent">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Contact Us</h5>
+                    <p class="grey-text text-lighten-4">(Email is probably the best way)</p>
+                    <p class="grey-text text-lighten-4">Phone: email first.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <a href="https://github.com/AndyResu"><i class="fab fa-github-square fa-5x" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+`;
+document.getElementById("footer").innerHTML = footer;
---
+++ header.js
@@ -0,0 +1,16 @@
+let header = `
+    <nav class="blue-grey darken-3 semi-transparent">
+        <div class="nav-wrapper">
+            <div class="header">
+                <a href="" id="title">Joan and Andy are Getting Married!</a>
+                <div class="header-right">
+                    <a href="">Home</a>
+                    <a href="/travel.html">Travel</a>
+                    <a href="/faq.html">FAQ</a>
+                    <a href="/photos.html">Photos</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+`;
+document.getElementById("header").innerHTML = header;
