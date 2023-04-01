c5f02abd146ccbfd05ecda3d2c9296b3ff239013
real name genij <zgxzgx@gmail.com>
2023-04-01 13:18:14 -0500

    feat(wedding): faq: accordian test again

--- footer.js
+++ footer.js
@@ -4,11 +4,7 @@ let footer = `
             <div class="row">
                 <div class="col l6 s12">
                     <h5 class="white-text">Contact Us</h5>
-                    <style type="text/css">
-                        span.spamprotection {display:none;}
-                    </style>
-                    <p class="grey-text text-lighten-4">AT<span class="spamprotection">JK<!-- abc@def -->Wedding2024@gma<!-- abc@def -->il.</span>com</p>
-                    // <p class="grey-text text-lighten-4">Phone: email first.</p>
+                    <p class="grey-text text-lighten-4">ATJK<!-- abc@def -->Wedding2024@gma<!-- abc@def -->il.com</p>
                 </div>
                 <div class="col l4 offset-l2 s12">
                     <h5 class="white-text">Links</h5>
